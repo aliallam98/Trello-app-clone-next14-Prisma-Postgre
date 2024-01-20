5b853e22e0bebc98523b990954f9743b81fae4d9
aliallam98 <allam98.ali@gmail.com>
2024-01-20 19:19:42 +0200

    added more logic to handle some cases and explain them by comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,39 @@
-import { authMiddleware } from "@clerk/nextjs";
+import {  authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth,req){
+
+    //in case already loged in and current url in public route ex.marketing page
+    //in case still not create an organization redirect user to create now one or select existing 
+    //if already selected redirect to organization page with this id
+    if(auth.userId  && auth.isPublicRoute ){
+      let path = "/select-org"
+
+      if(auth.userId && auth.orgId){
+        path = `/organization/${auth.orgId}`
+      }
+       const orgSelection = new URL(path,req.url)
+       return NextResponse.redirect(orgSelection)
+    }
+
+
+    // in case user not loged in and trying access private route
+    // redirect him to signin page and redirect him again to the same url that he was trying to access it
+    if(!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+
+  }
 });
 
 export const config = {
